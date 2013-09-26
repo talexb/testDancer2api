7e08b6a9d46eb8ceec3494ee55864efecf2eda2f
Alex Beamish <talexb@gmail.com>
2013-09-26 10:54:10 -0400

    First cut of attendance CSV script
    This is going to read the spreadsheet with x's (this transcribes the
    sheet I have on my clipboard) and puts that information into the
    database.

---
+++ csv3.pl
@@ -0,0 +1,102 @@
+
+#!/usr/bin/perl
+
+#  Read the Roster CSV containing information up to Sept 23 (for somereason the
+#  filename ends with 0930 -- no idea why).
+
+#  This will assume that the year is 2013, and will check for the existence of
+#  the events in the database. if they're missing, we'll exit (for now). If all
+#  of the events exist, we'll enter the attendance information. And this
+#  reminds me that we also need to enter hiatus information, and perhaps show
+#  that as a separate field in the roster that I track attendance. --Alex
+#  Beamish, September 26, 2013
+
+use strict;
+use warnings;
+use autodie;
+use Carp;
+
+use Text::CSV;
+use DBI;
+
+my $dataFile = "api/TNL-Roster-NameOnly-2013-SeptOct-930.csv";
+
+{
+    my $csv = Text::CSV->new( { binary => 1 } );
+    open( my $fh, '<encoding(utf8)', $dataFile );
+
+    #  Look at the first line, which will have the 'Name' column header,
+    #  followed by the five dates that this attendance roster is for.
+
+    my $firstLine = $csv->getline($fh);
+    my @dates = map { "2013-$firstLine->[$_]" } ( 1 .. 5 );
+
+    my $dbh = DBI->connect( 'dbi:SQLite:dbname=tnl1.db', '', '' );
+    defined $dbh or croak "Unable to connect to database " . $dbh->errstr;
+
+    my $searchEventCmd = "SELECT e_id FROM event WHERE e_date=?";
+
+    my $sthSearchEvent = $dbh->prepare($searchEventCmd);
+    defined $sthSearchEvent
+      or croak "Failed to prepare statement " . $sthSearchEvent->errstr;
+
+    my ( @errors, %dateEvent );
+    foreach my $date (@dates) {
+
+        $sthSearchEvent->execute($date)
+          or croak "Failed to execute " . $sthSearchEvent->errstr;
+        my $href = $sthSearchEvent->fetchrow_hashref;
+        if ( defined $href ) {
+
+            $dateEvent{$date} = $href->{e_id};
+
+        }
+        else {
+
+            push( @errors, "Unable to find event on date $date" );
+        }
+    }
+    $sthSearchEvent->finish
+      or croak "Failed to finish " . $sthSearchEvent->errstr;
+
+    if (@errors) {
+
+        print "There were missing dates as follows:\n--> ";
+        print join( "\n--> ", @errors ) . "\n";
+
+        exit;
+    }
+
+    #  OK -- all of the events on the roster exist in the database. Now we can
+    #  cycle through the names and check that the people exist in the database.
+
+    my $searchPersonCmd =
+      "SELECT p_id FROM person WHERE p_firstName=? AND p_lastName=?";
+
+    my $sthSearchPerson = $dbh->prepare($searchPersonCmd);
+    defined $sthSearchPerson
+      or croak "Failed to prepare statement " . $sthSearchPerson->errstr;
+
+    while ( my $line = $csv->getline($fh) ) {
+
+        my (@names) = map { $line->[$_] } ( 0, 7 );
+        foreach my $name (@names) {
+
+            my @subNames = split( /\s/, $name, 2 );
+            $sthSearchPerson->execute(@subNames)
+              or croak "Failed to execute " . $sthSearchPerson->errstr;
+
+            my $href = $sthSearchPerson->fetchrow_hashref;
+            if ( !defined $href ) {
+
+                print "ERROR: Did not find $name in database.\n";
+            }
+        }
+    }
+
+    $sthSearchPerson->finish
+      or croak "Failed to finish " . $sthSearchPerson->errstr;
+    $dbh->disconnect or croak "Failed to disconnect " . $dbh->errstr;
+
+    close($fh);
+}
